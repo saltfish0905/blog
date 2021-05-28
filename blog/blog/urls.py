03254a666876beb96862a6511a1bb75836c4e6d3
saltfish0905 <975285932@qq.com>
2021-05-29 02:00:18 +0800

    '日志功能'

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+def log(request):
+
+    return HttpResponse('test')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',log),
 ]
