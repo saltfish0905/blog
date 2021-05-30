08f2e93fe1112538940080cf5e8e78cf1ceba68c
saltfish0905 <975285932@qq.com>
2021-05-30 17:17:55 +0800

    '图片验证码'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -190,3 +190,7 @@ LOGGING={
         },
     }
 }
+
+#替换系统的User，使用自己定义的User
+#配置信息为‘子应用名.模型类型’
+AUTH_USER_MODEL='users.User'
--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,7 +1,10 @@
 #进行users子应用的视图路由
 from django.urls import path
-from users.views import RegisterView
+from users.views import ImageCodeView, RegisterView
 urlpatterns=[
     #path第一个参数为路由,第二个参数为视图函数名
     path('register/',RegisterView.as_view(),name='register'),
+
+    #图片验证码路由
+    path('imagecode/',ImageCodeView.as_view(),name='imagecode')
 ]
